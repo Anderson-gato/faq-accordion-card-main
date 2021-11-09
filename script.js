f71dda94d6b9a49bc61587da8e363b8a8008f6a4
Anderson Vieira <andersonvieira818@yahoo.com.br>
2021-11-09 00:56:17 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 const questParagraphs = document.querySelector('.questions p')
 const arrowUp = document.querySelectorAll('.questions .arrows')
 const paragraphs = document.querySelectorAll('.questions .paragraphs')
-
-// question.addEventListener('click', function () {
-//   const idParagraph = this.getAttribute('data-paragraph')
-//   const elementParagraph = document.querySelector(idParagraph)
-//   elementParagraph.classList.toggle('active')
-//   return console.log(idParagraph, elementParagraph)
-// })
-
 const quest = document.querySelectorAll('.questions > .question')
 const questions = document.querySelectorAll('.questions')
 
+// event para abrir as respostas
 questions.forEach(function (question) {
   question.addEventListener('click', function () {
     question.children[1].classList.toggle('active')
   })
 })
 
-quest.forEach(function (textBold) {
-  textBold.addEventListener('click', function () {
-    textBold.children[0].classList.toggle('bold-text')
-    textBold.children[1].classList.toggle('arrow-up')
+// quando for disparado o evento click, vai deixar colocar o bold no texto e a seta pra cima
+quest.forEach(function (textBoldAndArrowUp) {
+  textBoldAndArrowUp.addEventListener('click', function () {
+    textBoldAndArrowUp.children[0].classList.toggle('bold-text')
+    textBoldAndArrowUp.children[1].classList.toggle('arrow-up')
   })
+})
+
+// trocando os src das imagens quando a viewport chegar em 1000px
+const image = document.querySelector('.main-image')
+const mql = window.matchMedia('(min-width: 1000px)')
+
+function screen(e) {
+  // quando mql.matches for maior que 1000px
+  if (e.matches) {
+    image.children[0].src = '/images/illustration-woman-online-desktop.svg'
+    image.children[1].src = '/images/bg-pattern-desktop.svg'
+  } else {
+    image.children[0].src = '/images/illustration-woman-online-mobile.svg'
+    image.children[1].src = '/images/bg-pattern-mobile.svg'
+  }
+}
+
+mql.addEventListener('change', screen)
+
+// realizando eventos enquanto a página carrega
+window.addEventListener('DOMContentLoaded', () => {
+  const width = window.innerWidth
+  const minWidth = 1000
+
+  // condicional para quando a width for maior que 1000px ela trocar os src das imagens
+  if (width >= minWidth) {
+    image.children[0].src = '/images/illustration-woman-online-desktop.svg'
+    image.children[1].src = '/images/bg-pattern-desktop.svg'
+  } else {
+    image.children[0].src = '/images/illustration-woman-online-mobile.svg'
+    image.children[1].src = '/images/bg-pattern-mobile.svg'
+  }
 })
